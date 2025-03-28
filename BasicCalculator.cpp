dba780c1e65524ba4de91a4aede4f53a27366660
Haaziq Ali <167455693+HAAZIQ-ALI@users.noreply.github.com>
2025-03-28 13:48:10 +0530

    Add files via upload

---
+++ BasicCalculator.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void add(int a, int b) {
+    cout << "Sum: " << a + b << endl;
+}
+
+void sub(int a, int b) {
+    cout << "Difference: " << a - b << endl;  
+}
+
+void mul(int a, int b) {
+    cout << "Product: " << a * b << endl;  
+}
+
+void division(int a, int b) {
+    if (b == 0) {
+        cout << "Can't divide by zero" << endl;  
+    } else {
+        cout << "Quotient: " << a / b << endl;
+    }
+}
+void square(int a ){
+    int x = a*a;
+    cout << x<<endl;
+}
+
+int main() {
+    int a = 0, b = 0; 
+    char choice1 ='y';
+
+    while(choice1 == 'y' || choice1 == 'Y'){
+        cout << "Do you want to continue? (y/n): ";
+        cin >> choice1;
+        cout << "Enter First Number: ";
+        cin >> a;
+        cout << "\nEnter Second Number: ";
+        cin >> b;
+        char choice ;
+        cout <<"Enter The Arithmatic operator for ex - + , -, * , ^ and / :";
+        cin >> choice;
+
+
+        switch (choice){
+            case '/':
+            division (a , b);
+            break;
+            case '-':
+            sub (a , b);
+            break;
+            case '*':
+            mul(a , b);
+            break;
+            case '^':
+            square(a);
+            break;
+            default:
+            add(a , b);
+            
+    
+        }
+        
+    }
+    return 0;
+}
